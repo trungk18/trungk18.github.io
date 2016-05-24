a01e5db33898c6ece4ec69e11b1655a2b1c253c5
trungk18 <trungk18@hotmail.com>
2016-05-24 09:58:15 +0800

    Update send Email

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,23 +10,15 @@ $(function() {
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
             
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: encodeURI("//formspree.io/trungk18@email.com"),
+                url: encodeURI("https://formspree.io/trungk18@email.com"),
                 type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    message: message
-                },
+                data: $('#contactForm').serialize(),
                 cache: false,
                 success: function() {
                     // Enable button & show success message

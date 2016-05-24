8ecfa6d8c7634e917b4b13e66ad79253078124f9
trungk18 <trungk18@hotmail.com>
2016-05-25 00:20:50 +0800

    Update send mail

--- js/contact_me.js
+++ js/contact_me.js
@@ -15,36 +15,36 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: encodeURI("https://formspree.io/trungk18@email.com"),
-                type: "POST",
-                data: $('#contactForm').serialize(),
-                dataType: 'json',
-                success: function(data) {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+            //$.ajax({
+            //    url: encodeURI("https://formspree.io/trungk18@email.com"),
+            //    type: "POST",
+            //    data: $('#contactForm').serialize(),
+            //    dataType: 'json',
+            //    success: function(data) {
+            //        // Enable button & show success message
+            //        $("#btnSubmit").attr("disabled", false);
+            //        $('#success').html("<div class='alert alert-success'>");
+            //        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //            .append("</button>");
+            //        $('#success > .alert-success')
+            //            .append("<strong>Your message has been sent. </strong>");
+            //        $('#success > .alert-success')
+            //            .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function(error) {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+            //        //clear all fields
+            //        $('#contactForm').trigger("reset");
+            //    },
+            //    error: function(error) {
+            //        // Fail message
+            //        $('#success').html("<div class='alert alert-danger'>");
+            //        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            //            .append("</button>");
+            //        $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+            //        $('#success > .alert-danger').append('</div>');
+            //        //clear all fields
+            //        $('#contactForm').trigger("reset");
+            //    },
+            //})
         },
         filter: function() {
             return $(this).is(":visible");

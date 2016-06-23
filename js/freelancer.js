1a3d6b856344819309919bf5a046f52c2b5efef7
trungk18 <trungk18@hotmail.com>
2016-06-23 22:35:41 +0800

    Update Skill and Community section

--- js/freelancer.js
+++ js/freelancer.js
@@ -4,34 +4,42 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $("body").on("click", ".page-scroll a", function(event) {
-        var $anchor = $(this);
-        $("html, body").stop().animate({
-            scrollTop: $($anchor.attr("href")).offset().top
-        }, 1500, "easeInOutExpo");
-        event.preventDefault();
+$(function () {
+    //Smooth scrolling when click to nav
+    $('.navbar-fixed-top li > a').click(function (e) {
+        e.preventDefault();
+        var curLink = $(this);
+        var scrollPoint = $(curLink.attr('href')).position().top;
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 500);
     });
-});
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
+    //Resize Header on scrolling
+    $(window).scroll(function () {
+        //Method 2: Using toggleClass
+        $(".navbar-default").toggleClass("navbar-shrink", $(this).scrollTop() > 50)
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $("body").scrollspy({
+        target: ".navbar-fixed-top"
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $(".navbar-collapse a").click(function () {
+        $(".navbar-toggle:visible").click();
+    });
+
+    // Floating label headings for the contact form
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function () {
         $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
+    }).on("blur", ".floating-label-form-group", function () {
         $(this).removeClass("floating-label-form-group-with-focus");
     });
 });
 
-// Highlight the top nav as scrolling occurs
-$("body").scrollspy({
-    target: ".navbar-fixed-top"
-});
 
-// Closes the Responsive Menu on Menu Item Click
-$(".navbar-collapse a").click(function() {
-    $(".navbar-toggle:visible").click();
-});
+
---
+++ js/main.js
@@ -0,0 +1,9 @@
+﻿var portfolioApp = angular.module('portfolioApp', []);
+
+portfolioApp.controller('MainCtrl', ['$scope','$http', function ($scope, $http) {
+    $http.get("https://api.github.com/users/trungk18/repos").then(function (response) {
+        $scope.repositoryList = response.data;
+    });
+}]);
+
+

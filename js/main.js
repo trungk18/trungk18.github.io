a9db6eea88a952f4ab44fb18ac1a15632f7ced3e
trungk18 <trungk18@hotmail.com>
2016-06-27 10:18:52 +0800

    Update JS

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 portfolioApp.controller('MainCtrl', ['$scope','$http', function ($scope, $http) {
     $http.get("https://api.github.com/users/trungk18/repos").then(function (response) {
         $scope.repositoryList = response.data;
-        console.log($scope.repositoryList);
     });
 }]);
 

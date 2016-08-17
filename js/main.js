e767a7271122b40e3c5b0bd24b474d9e58695ebb
Vo Tuan Trung <trungk18@gmail.com>
2016-08-18 00:39:30 +0800

    Update github repo fetch

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 ﻿var portfolioApp = angular.module('portfolioApp', []);
 
-portfolioApp.controller('MainCtrl', ['$scope','$http', function ($scope, $http) {
+portfolioApp.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
     $http.get("https://api.github.com/users/trungk18/repos").then(function (response) {
-        $scope.repositoryList = response.data;
+        if (!response) return;
+        $scope.repositoryList = response.data.sort(function (a, b) {
+            return b.stargazers_count - a.stargazers_count;
+        });
+
     });
 }]);
 

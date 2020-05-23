142cad2b1363b2f3599f9ff9dd1c52240311df95
Trung Vo <trung.vo@zyllem.com>
2020-05-23 16:45:20 +0800

    Update toggle dark theme

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,26 +1,83 @@
 var DarkMode = (function () {
-  const DARK_MODE_QUERY = "(prefers-color-scheme: dark)";
-  const DARK_CLASS = "dark";
-  function isDark() {
+  var DARK_MODE_LOCAL_STORAGE = "TRUNGK18_BLOG_DARK_MODE"
+  var DARK_MODE_QUERY = "(prefers-color-scheme: dark)";
+  var MOON_ICON = "fa-moon-o";
+  var SUN_ICON = "fa-sun-o";
+  var DARK_CLASS = "dark";
+
+  var storage = {
+    isDark: function () {
+      try {
+        return !!localStorage.getItem(DARK_MODE_LOCAL_STORAGE);
+      } catch (error) {
+        return false;
+      }
+    },
+    setDarkTheme: function (isDark) {
+      try {
+        localStorage.setItem(DARK_MODE_LOCAL_STORAGE, isDark ? DARK_CLASS : "");
+      } catch (error) {
+
+      }
+    }
+  }
+
+  function isDarkFromSystem() {
     return window.matchMedia && window.matchMedia(DARK_MODE_QUERY).matches;
+  }
+
+  function isDark() {
+    if (storage.isDark()) {
+      return true;
+    }
+    return isDarkFromSystem();
+  }
+
+  function toggleIconManually(isDark) {
+    var html = document.getElementsByTagName("html")[0].classList;
+    var icon = getIcon();
+    if (!icon) {
+      return;
+    }
+    storage.setDarkTheme(isDark);
+    if (isDark) {
+      icon.classList.remove(MOON_ICON);
+      icon.classList.add(SUN_ICON);
+      html.add(DARK_CLASS)
+    }
+    else {
+      icon.classList.remove(SUN_ICON)
+      icon.classList.add(MOON_ICON);
+      html.remove(DARK_CLASS)
+    }
   }
 
   function watch(fn) {
     window.matchMedia &&
       window.matchMedia(DARK_MODE_QUERY).addEventListener("change", (e) => {
-        const isDarkMode = e.matches;
+        var isDarkMode = e.matches;
         fn && fn(isDarkMode);
       });
   }
 
+  function getIcon() {
+    return document.getElementById("DarkThemeToggleIcon");
+  }
+
   function init() {
-    var html = document.getElementsByTagName("html")[0].classList;
-    if (isDark()) {
-      html.add(DARK_CLASS);
-    }
+    var iconContainer = document.getElementById("DarkThemeToggleContainer");
+
+    toggleIconManually(isDark());
+
+    iconContainer.addEventListener("click", function () {
+      let isDark = storage.isDark();
+      toggleIconManually(!isDark);
+    })
+
     watch(function (isDarkMode) {
-      isDarkMode ? html.add(DARK_CLASS) : html.remove(DARK_CLASS);
+      toggleIconManually(isDarkMode);
     });
+
   }
 
   init();
